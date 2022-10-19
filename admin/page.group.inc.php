0488f75ee1d90270e999da277fe0076d654db250
Orrelios <49332652+Orrelios@users.noreply.github.com>
2022-10-19 16:19:17 -0500

    Update page.group.inc.php

--- admin/page.group.inc.php
+++ admin/page.group.inc.php
@@ -1,14 +1,6 @@
 <?php
 
-    /*!
-     * ifsoft.co.uk
-     *
-     * http://ifsoft.com.ua, https://ifsoft.co.uk, https://raccoonsquare.com
-     * raccoonsquare@gmail.com
-     *
-     * Copyright 2012-2020 Demyanchuk Dmitry (raccoonsquare@gmail.com)
-     */
-
+    
     if (!defined("APP_SIGNATURE")) {
 
         header("Location: /");
